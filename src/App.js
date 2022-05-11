f98ce6ad179a2e2caff576b61bda5b20c6e72302
IbraheemShaban <ishaban097@gmail.com>
2022-05-11 20:05:54 +0300

    FINISHED the game

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-
 import './App.css';
 import Header from './Components/Header';
 import IslandForm from './Components/IslandForm';
 import IslandList from './Components/IslandList';
-
+import islands from './data/islands';
+import { useState } from 'react';
 
 function App() {
-
-  let island = {
-    name: "Boubyan",
-    img: "http://photos.wikimapia.org/p/00/02/20/48/92_big.jpg"
-  }
-
+  let initialIsland = {
+    name: 'Boubyan',
+    img: 'http://photos.wikimapia.org/p/00/02/20/48/92_big.jpg',
+  };
+  const [island, setIsland] = useState(initialIsland);
+  const [islandsState, setIslandsState] = useState(islands);
+  const incVisitors = (id) => {
+    let newIslands = islandsState;
+    newIslands = islandsState.find((island) => island.id == id);
+    newIslands.visitors++;
+    setIslandsState(newIslands);
+  };
   return (
-
     <div>
       <Header />
 
       <div className="homePage">
-        <IslandList />
-        <IslandForm island={island} />
+        <IslandList setIsland={setIsland} islands={islandsState} />
+        <IslandForm island={island} incVisitors={incVisitors} />
       </div>
     </div>
   );
